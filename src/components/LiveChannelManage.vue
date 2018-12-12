9a4194ec92ad9aef5b3cdda1c97f87488f164f29
Elite <elicloudh@gmail.com>
2018-12-12 14:56:21 +0500

    fixing ambigous stream fps bug

--- src/components/LiveChannelManage.vue
+++ src/components/LiveChannelManage.vue
@@ -393,7 +393,9 @@ export default {
 
       _.each(codecs, codec => {
         if (!/^v/gi.test(codec.id) || _.isNil(codec.fps)) return;
-        this.streamFps = Math.round(codec.fps);
+        let fps = Math.round(codec.fps)
+        if (fps <= 144)
+          this.streamFps = fps;
       });
     },
     unsubscribeMediaPulse() {
